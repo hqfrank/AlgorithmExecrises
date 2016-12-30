8489cf9b818a9862b8f77d40523088ff93b309ba
Qiang Hu <Qiang Hu>
2016-12-30 07:02:04 -0500

    Solution of 1.2.5

---
+++ Print125.java
@@ -0,0 +1,13 @@
+import edu.princeton.cs.algs4.StdOut;
+
+public class Print125{
+	public static void main(String[] args){
+		String s = "Hello World";
+		s.toUpperCase();
+		s.substring(6,11);
+		StdOut.println(s);
+		s = s.toUpperCase();
+		s = s.substring(6,11);
+		StdOut.println(s);
+	}
+}
