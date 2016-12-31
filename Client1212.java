90ce57a470a4cb8c1ff2dac2ac8d36c04de61eb4
Qiang Hu <Qiang Hu>
2016-12-31 08:00:13 -0500

    Solution of 1.2.12

---
+++ Client1212.java
@@ -0,0 +1,9 @@
+public class Client1212{
+	public static void main(String[] args){
+		int m = Integer.parseInt(args[0]);
+		int d = Integer.parseInt(args[1]);
+		int y = Integer.parseInt(args[2]);
+		SmartDate date = new SmartDate(m,d,y);
+		System.out.println(date);
+	}
+}
--- SmartDate.java
+++ SmartDate.java
@@ -50,8 +50,47 @@ public class SmartDate{
 		return year;
 	}
 
+	public String dayOfTheWeek(){
+		if(year < 2000){
+			return "It's not a date in 21st Century.";
+		}
+		else{
+			// 1/1/2000 is Saturday
+			int totalyearPrev = year - 2001;
+			int totalLeapYears = totalyearPrev / 4 - totalyearPrev / 100 + totalyearPrev / 400 +1;
+			int totalDatePrevYears = (totalyearPrev+1) * 365 + totalLeapYears;
+			if(year == 2000) totalDatePrevYears = 0;
+			int totalDate = totalDatePrevYears;
+			int totalMonthPrev = month - 1;
+			int totalDatePrevMonth = totalMonthPrev * 31;
+			if(year % 400 == 0 || (year % 4 == 0 && year % 100 != 0)){
+				if(month == 3 || month == 4) totalDatePrevMonth -= 2;
+				if(month == 5 || month == 6) totalDatePrevMonth -= 3;
+				if(month == 7 || month == 8 || month == 9) totalDatePrevMonth -= 4;
+				if(month == 10 || month == 11) totalDatePrevMonth -= 5;
+				if(month == 12) totalDatePrevMonth -= 6;
+			}
+			else{
+				if(month == 3 || month == 4) totalDatePrevMonth -= 3;
+				if(month == 5 || month == 6) totalDatePrevMonth -= 4;
+				if(month == 7 || month == 8 || month == 9) totalDatePrevMonth -= 5;
+				if(month == 10 || month == 11) totalDatePrevMonth -= 6;
+				if(month == 12) totalDatePrevMonth -= 7;
+			}
+			totalDate = totalDate + totalDatePrevMonth + day;
+			if(totalDate % 7 == 0) return "Friday";
+			if(totalDate % 7 == 1) return "Saturday";
+			if(totalDate % 7 == 2) return "Sunday";
+			if(totalDate % 7 == 3) return "Monday";
+			if(totalDate % 7 == 4) return "Tuesday";
+			if(totalDate % 7 == 5) return "Wednesday";
+			if(totalDate % 7 == 6) return "Thursday";
+			return "Error!";
+		}
+	}
+
 	public String toString(){
-		return month() + "/" + day() + "/" + year();
+		return month() + "/" + day() + "/" + year() + " " + dayOfTheWeek();
 	}
 
 	public boolean equals(Object x){
