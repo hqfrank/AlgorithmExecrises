8bffda9422ddad1a4475f7a737091cd3251c36d7
Qiang Hu <Qiang Hu>
2016-12-30 21:54:31 -0500

    Solution of 1.2.8

---
+++ SwapArray128.java
@@ -0,0 +1,6 @@
+public class SwapArray128{
+	public static void main(String[] args){
+		System.out.println("int[] t = a; a = b; b = t;");
+		System.out.println("The above code efficiently swap arrays a and b in the way of swapping references.");
+	}
+}
