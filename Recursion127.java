8e47c4e6e5bc87f9fe4526209f628e9d0c91e323
Qiang Hu <Qiang Hu>
2016-12-30 21:49:47 -0500

    Solution of 1.2.7. The method returns the reversed version of the input string.

---
+++ Recursion127.java
@@ -0,0 +1,13 @@
+public class Recursion127{
+	public static String mystery(String s){
+		int N = s.length();
+		if (N <= 1) return s;
+		String a = s.substring(0,N/2);
+		String b = s.substring(N/2,N);
+		return mystery(b) + mystery(a);
+	}
+	 public static void main(String[] args){
+	 	System.out.println("The original string is "+ args[0]);
+	 	System.out.println("The returned string is "+ mystery(args[0]));
+	 }
+}
